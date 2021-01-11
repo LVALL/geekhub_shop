4c45d510a4c4d322bfef9959ad3982413e8688db
LVALL <valeradracenko418@gmail.com>
2021-01-11 21:25:55 +0200

    Add products. Filter by category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,9 @@
 class ProductsController < ApplicationController
-  def index; end
+  def index
+    @products = Product.all.order(created_at: :desc)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'products#index'
+
+  resources :categories, only: :show do
+    resources :products, only: %i[index show]
+  end
 end
