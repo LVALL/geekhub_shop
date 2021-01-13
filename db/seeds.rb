c58442d58084d2f13a343dc99750dc85ab40dcde
LVALL <valeradracenko418@gmail.com>
2021-01-13 22:43:58 +0200

    Add test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name: 'Mobile phones')
+Category.create(name: 'Games')
+Category.create(name: 'PC')
+Product.create(name: 'Iphone 5', description: 'The iPhone is a line of smartphones designed and marketed by Apple Inc.
+that use Apple`s iOS mobile operating system. The first-generation iPhone was announced by former Apple CEO Steve Jobs
+on January 9, 2007.', image: 'https://www.applegazette.com/wp-content/uploads/2019/09/iphone-4408926_960_720-700x400.jpg',
+               price: 120.59, category_id: 1)
