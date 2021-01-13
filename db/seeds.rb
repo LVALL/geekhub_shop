1da183a17624418744d8d917a707a01fe6e2cf07
LVALL <valeradracenko418@gmail.com>
2021-01-13 23:10:00 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,70 @@ Product.create(name: 'Iphone 5', description: 'The iPhone is a line of smartphon
 that use Apple`s iOS mobile operating system. The first-generation iPhone was announced by former Apple CEO Steve Jobs
 on January 9, 2007.', image: 'https://www.applegazette.com/wp-content/uploads/2019/09/iphone-4408926_960_720-700x400.jpg',
                price: 120.59, category_id: 1)
+Product.create(name: 'Iphone 6', description: 'The iPhone is a line of smartphones designed and marketed by Apple Inc.
+that use Apple`s iOS mobile operating system. The first-generation iPhone was announced by former Apple CEO Steve Jobs
+on January 9, 2007.', image: 'https://cdn.newsledge.com/wp-content/uploads/2014/09/iphone6.jpg',
+               price: 250.99, category_id: 1)
+Product.create(name: 'Iphone 7', description: 'The iPhone is a line of smartphones designed and marketed by Apple Inc.
+that use Apple`s iOS mobile operating system. The first-generation iPhone was announced by former Apple CEO Steve Jobs
+on January 9, 2007.', image: 'https://tehnot.com/wp-content/uploads/2016/09/Nita-9-A-57Full.jpg',
+               price: 349.99, category_id: 1)
+Product.create(name: 'Iphone 8', description: 'The iPhone is a line of smartphones designed and marketed by Apple Inc.
+that use Apple`s iOS mobile operating system. The first-generation iPhone was announced by former Apple CEO Steve Jobs
+on January 9, 2007.', image: 'https://i.playground.ru/p/c8CD0YDN4vmdsLsOp6hzYA.jpeg',
+               price: 469.99, category_id: 1)
+Product.create(name: 'Iphone X', description: 'The iPhone is a line of smartphones designed and marketed by Apple Inc.
+that use Apple`s iOS mobile operating system. The first-generation iPhone was announced by former Apple CEO Steve Jobs
+on January 9, 2007.', image: 'https://lh6.googleusercontent.com/CZK6l7EnhsqLo_E87cQ1i-e0A46DpT6NLwTzkBjbsl8mr4yYcjIHauvQ5AVuZsowYBY3aW9YzCcwLbXHJfJopcvbQHPzrBgnjKp76QwOHJIsuAllRSsxhfeuQIT54P8ijkP_pCQA',
+               price: 659.99, category_id: 1)
+Product.create(name: 'Iphone 11', description: 'The iPhone is a line of smartphones designed and marketed by Apple Inc.
+that use Apple`s iOS mobile operating system. The first-generation iPhone was announced by former Apple CEO Steve Jobs
+on January 9, 2007.', image: 'https://phonewale.com/reviews/160222967357433.jpeg',
+               price: 959.99, category_id: 1)
+Product.create(name: 'Counter Strike', description: 'It is the fourth game in the Counter-Strike series and was released for
+Windows, macOS, Xbox 360, and PlayStation 3 in August 2012, while the Linux version was released in 2014.',
+               image: 'https://igamesworld.ru/uploads/posts/2016-09/medium/1472758064_p1_2041740_e1ca1c0a.png',
+               price: 15.99, category_id: 2)
+Product.create(name: 'Mortal Kombat', description: 'Mortal Kombat Tournament, the featured fighting tournament and
+namesake of the Mortal Kombat series of fighting games.',
+               image: 'https://gamingapez.com/wp-content/uploads/2017/09/mortal-kombat-2-700x400.jpg',
+               price: 25.99, category_id: 2)
+Product.create(name: 'Fallout 4', description: 'Fallout 4 is an action role-playing game developed by Bethesda Game
+Studios and published by Bethesda Softworks. It is the fourth main game in the Fallout series and was released worldwide
+on November 10, 2015.',
+               image: 'https://igamesworld.ru/uploads/posts/2016-03/medium/1458371190_1.1.png',
+               price: 30.99, category_id: 2)
+Product.create(name: 'CyberPunk 2077', description: 'Cyberpunk 2077 is a 2020 action role-playing video game developed
+and published by CD Projekt. The story takes place in Night City, an open world set in the Cyberpunk universe.',
+               image: 'https://www.parlortalk.com/wp-content/uploads/2020/09/CyberPunk-2077.jpg',
+               price: 40.99, category_id: 2)
+Product.create(name: 'Red Dead Redemption 2', description: 'Red Dead Redemption 2 is a 2018 action-adventure game
+developed and published by Rockstar Games. The game is the third entry in the Red Dead series and is a prequel to
+the 2010 game Red Dead Redemption.',
+               image: 'https://igamesworld.ru/uploads/posts/2016-10/medium/1477586063_reddead2-1.jpg',
+               price: 47.99, category_id: 2)
+Product.create(name: 'Computer 1', description: 'Personal computers are intended to be operated directly by an end user,
+rather than by a computer expert or technician. Unlike large, costly minicomputers and mainframes, time-sharing by many
+people at the same time is not used with personal computers.',
+               image: 'https://i.pinimg.com/736x/7d/25/ef/7d25efa643f19985e164906c3cebfefb.jpg',
+               price: 700.99, category_id: 3)
+Product.create(name: 'Computer 2', description: 'Personal computers are intended to be operated directly by an end user,
+rather than by a computer expert or technician. Unlike large, costly minicomputers and mainframes, time-sharing by many
+people at the same time is not used with personal computers.',
+               image: 'https://186581-550669-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/06/CORSAIR-Vengeance-5185-Gaming-PC.png',
+               price: 860.99, category_id: 3)
+Product.create(name: 'Computer 3', description: 'Personal computers are intended to be operated directly by an end user,
+rather than by a computer expert or technician. Unlike large, costly minicomputers and mainframes, time-sharing by many
+people at the same time is not used with personal computers.',
+               image: 'https://i.pinimg.com/736x/5d/86/26/5d862682b8698cdf552d48246545a99c.jpg',
+               price: 660.99, category_id: 3)
+Product.create(name: 'Computer 4', description: 'Personal computers are intended to be operated directly by an end user,
+rather than by a computer expert or technician. Unlike large, costly minicomputers and mainframes, time-sharing by many
+people at the same time is not used with personal computers.',
+               image: 'https://www.caffeinatedgamer.com/wp-content/uploads/2020/04/valorant-best-gaming-pc.jpg',
+               price: 999.99, category_id: 3)
+Product.create(name: 'Computer 5', description: 'Personal computers are intended to be operated directly by an end user,
+rather than by a computer expert or technician. Unlike large, costly minicomputers and mainframes, time-sharing by many
+people at the same time is not used with personal computers.',
+               image: 'https://186581-550669-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/01/Corsair-One-i164-Compact-Gaming-PC.jpg',
+               price: 567.99, category_id: 3)
