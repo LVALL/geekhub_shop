e7364377fa2e42213b67b97bfcda4c5ddbdff83d
LVALL <valeradracenko418@gmail.com>
2021-01-13 22:05:56 +0200

    Add filters

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,22 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc).paginate(page: params[:page], per_page: 3)
+    sort
+  end
+
+  private
+
+  def sort
+    @products = if params[:filter] == 'LowPrice'
+                  @category.products.order(price: :asc).paginate(page: params[:page], per_page: 3)
+                elsif params[:filter] == 'HighPrice'
+                  @category.products.order(price: :desc).paginate(page: params[:page], per_page: 3)
+                elsif params[:filter] == 'Alphabetical'
+                  @category.products.order(name: :asc).paginate(page: params[:page], per_page: 3)
+                elsif params[:min].present? || params[:max].present?
+                  @category.products.where(price: [params[:min].to_i].first..[params[:max].to_i].last).paginate(page: params[:page], per_page: 3)
+                else
+                  @category.products.order(created_at: :desc).paginate(page: params[:page], per_page: 3)
+                end
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all.order(created_at: :desc).paginate(page: params[:page], per_page: 6)
+    sort
   end
 
   def show
@@ -14,5 +14,21 @@ class ProductsController < ApplicationController
   def search
     @products = Product.where('name ILIKE ?', "%#{params[:q]}%").paginate(page: params[:page], per_page: 3)
     render :index
+  end
+
+  private
+
+  def sort
+    @products = if params[:filter] == 'LowPrice'
+                  Product.all.order(price: :asc).paginate(page: params[:page], per_page: 6)
+                elsif params[:filter] == 'HighPrice'
+                  Product.all.order(price: :desc).paginate(page: params[:page], per_page: 6)
+                elsif params[:filter] == 'Alphabetical'
+                  Product.all.order(name: :asc).paginate(page: params[:page], per_page: 6)
+                elsif params[:min].present? || params[:max].present?
+                  Product.where(price: [params[:min].to_i].first..[params[:max].to_i].last).paginate(page: params[:page], per_page: 6)
+                else
+                  Product.all.order(created_at: :desc).paginate(page: params[:page], per_page: 6)
+                end
   end
 end
