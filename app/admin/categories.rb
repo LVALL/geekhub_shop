f268325ebb60693ea128ca3541b848c9ca078c43
LVALL <valeradracenko418@gmail.com>
2021-01-19 18:03:52 +0200

    add to active admin subcategory create method

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Category do
-  permit_params :name
+  permit_params :name, :parent_id
 
   index do
     selectable_column
@@ -18,6 +18,7 @@ ActiveAdmin.register Category do
   form do |f|
     f.inputs do
       f.input :name
+      f.input :parent_id
     end
     f.actions
   end
