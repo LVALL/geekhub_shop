906e115f3533544d5911a5e961f01fcf1948516c
LVALL <valeradracenko418@gmail.com>
2021-01-22 14:50:29 +0200

    Add image uploader

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :order_items
 
+  mount_uploader :image, ImageUploader
   validates_presence_of :name, :price, :category
 
   def to_param
