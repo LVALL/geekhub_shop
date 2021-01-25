a68b3449534126cf2f73b5049e2c69fd264ad59e
LVALL <valeradracenko418@gmail.com>
2021-01-25 18:10:25 +0200

    finish comments

--- app/javascript/js/rate_with_stars.js
+++ app/javascript/js/rate_with_stars.js
@@ -1,9 +1,15 @@
 document.addEventListener('turbolinks:load', function () {
     if (window.location.pathname.includes("products")) {
+        $('.update-comment-form').hide();
+
         $('.rating').each(function () {
             $(this).children('.rating__check').bind( "click", function() {
                 $('.rating-field').val(parseInt($(this).val()));
             });
+        });
+
+        $('.toggle-edit-btn').click(function() {
+            $('.update-comment-form').toggle(500);
         });
     }
 });
