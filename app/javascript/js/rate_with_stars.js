a0363f2b7c387bc38dbc66908f2852d89f6c79e8
LVALL <valeradracenko418@gmail.com>
2021-01-25 12:57:14 +0200

    refactor views

--- app/javascript/js/rate_with_stars.js
+++ app/javascript/js/rate_with_stars.js
@@ -1,7 +1,7 @@
 document.addEventListener('turbolinks:load', function () {
     if (window.location.pathname.includes("products")) {
-        $('.raiting').each(function () {
-            $(this).children('.raiting__check').bind( "click", function() {
+        $('.rating').each(function () {
+            $(this).children('.rating__check').bind( "click", function() {
                 $('.rating-field').val(parseInt($(this).val()));
             });
         });
