c5fc415f3d17feba8d36150d8e52510ba6ed1ab7
LVALL <valeradracenko418@gmail.com>
2021-01-30 14:05:13 +0200

    change registrations config

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,4 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_action :verify_authenticity_token
-
   def github
     @user = User.from_omniauth(request.env['omniauth.auth'])
     sign_in_and_redirect @user
